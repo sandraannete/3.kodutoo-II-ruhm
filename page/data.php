d1ed52be4dfcdcf687934031678a1adeb015dc7b
sandtaga <sandtaga@greeny.cs.tlu.ee>
2017-01-09 20:10:22 +0200

    dk

---
+++ page/data.php
@@ -0,0 +1,43 @@
+<?php 
+	
+	require("../functions.php");
+	
+	//kui ei ole kasutaja id'd
+	if (!isset($_SESSION["userId"])) {
+		
+		//suunan sisselogimise lehele
+		header("Location: login.php");
+		exit();
+		
+	}
+	//kui on ?logout aadressi real siis login välja
+	if(isset ($_GET["logout"])) {
+		
+		session_destroy();
+		header("Location:login.php");
+		exit();
+	}
+	$msg = " ";
+	if(isset($_SESSION["message"])) {
+		$msg = $_SESSION["message"];
+	
+		//kui ühe näitame siis kusutua ära, et pärast refreshi ei näita
+	unset($_SESSION["message"]);
+	
+		}
+	
+?>
+<?php require("../header.php"); ?>
+<div class="container">
+	<?=$msg;?>
+
+	<p><a href="cars.php"> Cars </a></p>
+	<a href="?logout=1"> Log out</a>
+
+
+	<br>
+	<br>
+	<br>
+	<br>
+</div>
+<?php require("../footer.php"); ?>
